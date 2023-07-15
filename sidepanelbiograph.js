031a8f41c917c924aaaf43211c7b7cab189c31b6
phamhoainhan <137066137+phamhoainhan@users.noreply.github.com>
2023-07-15 13:54:09 +0700

    Add files via upload

---
+++ sidepanelbiograph.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "230px";
+    document.getElementById("mySidepanel").style.height = "100%";
+
+}
+function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+
+}
