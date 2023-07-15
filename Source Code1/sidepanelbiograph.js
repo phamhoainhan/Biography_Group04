4b3bf3b447e5b4fee066a584c7c0c04ed565f9f8
phamhoainhan <137066137+phamhoainhan@users.noreply.github.com>
2023-07-15 12:08:21 +0700

    Delete Source Code1 directory

--- Source Code1/sidepanelbiograph.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("mySidepanel").style.width = "230px";
-    document.getElementById("mySidepanel").style.height = "100%";
-
-}
-function closeNav() {
-    document.getElementById("mySidepanel").style.width = "0";
-
-}
